39c55bd10426ac274c3ff9423943a6988acac52b
Ahmed-Mahdi0 <ahmeedsayed01@gmail.com>
2023-10-20 23:06:42 +0300

    added register and login feateurs and image upload

--- accounts/forms.py
+++ accounts/forms.py
@@ -15,17 +15,20 @@ def validate_phone_number(value):
 
 
 class RegisterForm(UserCreationForm):
-    phoneNumber = forms.CharField(max_length=20, validators=[validate_phone_number])
-    image = forms.ImageField()
+    phoneNumber = forms.CharField(max_length=20, validators=[validate_phone_number],widget=forms.TextInput(attrs={'class': 'form-control'}))
+    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
         fields = ("username", "email", "phoneNumber", "password1", "password2", "image")
+        
 
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=False)
         user.email = self.cleaned_data["email"]
-
+        for field in self.fields:
+            field.widget=forms.TextInput(attrs={'class':'form-control'})
+            
         if commit:
             user.save()
         user_profile = UserProfile(
