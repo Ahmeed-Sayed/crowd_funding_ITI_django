86dc36864c9ead5b946048c0fe301c509c654f72
Ahmed-Mahdi0 <ahmeedsayed01@gmail.com>
2023-10-23 00:59:46 +0300

    completed authentication logic

--- accounts/models.py
+++ accounts/models.py
@@ -4,8 +4,10 @@ from django.db import models
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phoneNumber = models.CharField(max_length=11,unique=True)
+    phoneNumber = models.CharField(max_length=11, unique=True)
     image = models.ImageField(null=True, blank=True, upload_to="images/")
+    birthdate = models.DateField(null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("login/", AccountLogin.as_view(), name="accountLogin"),
     path("logout/", accountLogout, name="accountLogout"),
     path("activate/<uidb64>/<token>", activate, name="activate"),
+    path('profile/<int:id>', profileView,name="profile"),
+    path('profile/<int:id>/edit',ProfileEditView.as_view(),name="profileEdit"),
+    path('profile/<int:id>/delete',profileDelete,name="profileDelete"),
 ]
