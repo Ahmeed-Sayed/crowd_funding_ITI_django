b605b0df37a24218505fbfc5e505a629141fb94e
Ahmed-Mahdi0 <ahmeedsayed01@gmail.com>
2023-10-23 21:28:34 +0300

    added project model and related models

---
+++ projects/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+admin.site.register(CategoriesModel)
+admin.site.register(TagsModel)
+admin.site.register(ProjectsModel)
+admin.site.register(CommentsModel)
+admin.site.register(UserProjectRating)
+admin.site.register(PictuersModel)
+admin.site.register(ProjectReportModel)
+admin.site.register(CommentReportModel)
---
+++ projects/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+from accounts.models import UserProfile
+from django.core.validators import MaxValueValidator
+
+class CategoriesModel(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class TagsModel(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class ProjectsModel(models.Model):
+    title = models.CharField(max_length=200)
+    details = models.TextField(max_length=500)
+    category = models.ManyToManyField(CategoriesModel)
+    target = models.IntegerField()
+    tags = models.ManyToManyField(TagsModel)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    donations = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.title
+
+
+class CommentsModel(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    text = models.TextField(max_length=500)
+    project = models.ForeignKey(
+        ProjectsModel, on_delete=models.CASCADE, related_name="comments"
+    )
+
+
+class UserProjectRating(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    project = models.ForeignKey(
+        ProjectsModel, on_delete=models.CASCADE, related_name="ratings"
+    )
+    rating = models.IntegerField(validators=[MaxValueValidator(5)])
+
+
+class PictuersModel(models.Model):
+    image = models.ImageField(upload_to="projectImages")
+    project = models.ForeignKey(
+        ProjectsModel, related_name="images", on_delete=models.CASCADE
+    )
+
+
+class ProjectReportModel(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    project = models.ForeignKey(
+        ProjectsModel, related_name="projectReports", on_delete=models.CASCADE
+    )
+    text = models.TextField(max_length=200)
+
+
+class CommentReportModel(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    project = models.ForeignKey(
+        ProjectsModel, related_name="commentReports", on_delete=models.CASCADE
+    )
+    text = models.TextField(max_length=200)
