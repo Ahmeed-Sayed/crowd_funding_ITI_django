22e6bcd7ee3aace6205e475d2dc6b11b60670aa5
Mostafa Nabieh <engaymanfarag1997@gmail.com>
2023-10-25 16:22:54 +0300

    Modified settings.py

--- project/settings.py
+++ project/settings.py
@@ -32,14 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "accounts",
-    'projects',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts",
+    'projects',
 ]
 
 MIDDLEWARE = [
