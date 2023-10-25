a7a1eb636f67ca0f5ab0188382e21aa62633186a
AhmedKhaled146 <a7med74yaso@gmail.com>
2023-10-26 01:21:31 +0300

    user can add 5 pictures and adjusted the create form

--- projects/forms.py
+++ projects/forms.py
@@ -1,29 +1,48 @@
+from django.utils import timezone
 from django import forms
-from .models import ProjectsModel, UserProjectRating, CommentsModel,DonationModel
+from .models import (
+    ProjectsModel,
+    UserProjectRating,
+    CommentsModel,
+    DonationModel,
+    PictuersModel,
+)
+
+from django.core.exceptions import ValidationError
+import os
+from django.core.files.images import get_image_dimensions
 
 
 class ProjectCreationForm(forms.ModelForm):
     start_time = forms.DateTimeField(
-        widget=forms.DateTimeInput(
-            attrs={"type": "datetime-local", "class": "form-control"}
-        ),
+        widget=forms.DateTimeInput(attrs={"type": "datetime-local"}),
     )
     end_time = forms.DateTimeField(
-        widget=forms.DateTimeInput(
-            attrs={"type": "datetime-local", "class": "form-control"}
-        ),
+        widget=forms.DateTimeInput(attrs={"type": "datetime-local"}),
     )
 
     class Meta:
         model = ProjectsModel
-        exclude = ("donations", "user")
+        exclude = ("donations", "user", "completed", "closed")
+
+    def clean_start_time(self):
+        start_time = self.cleaned_data.get("start_time")
+        if start_time and start_time < timezone.now():
+            raise ValidationError("Start date should be today or later.")
+        return start_time
+
+    def clean_end_time(self):
+        end_time = self.cleaned_data.get("end_time")
+        start_time = self.cleaned_data.get("start_time")
+        if end_time and start_time and end_time <= start_time:
+            raise ValidationError("End date should be after the start date.")
+        return end_time
 
     def __init__(self, *args, **kwargs):
         super(ProjectCreationForm, self).__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].widget.attrs.update({"class": "form-control"})
-
-
+        
 class CommentForm(forms.ModelForm):
     class Meta:
         model = CommentsModel
@@ -34,19 +53,50 @@ class CommentForm(forms.ModelForm):
         self.fields["text"].widget = forms.TextInput(attrs={"class": "form-control"})
 
 
-
 class RatingForm(forms.ModelForm):
     class Meta:
         model = UserProjectRating
-        fields = ['rating']
+        fields = ["rating"]
+
     def __init__(self, *args, **kwargs):
         super(RatingForm, self).__init__(*args, **kwargs)
-        self.fields["rating"].widget = forms.NumberInput(attrs={"class": "form-control"})
+        self.fields["rating"].widget = forms.NumberInput(
+            attrs={"class": "form-control"}
+        )
+
 
 class DonationForm(forms.ModelForm):
     class Meta:
-        model=DonationModel
-        fields=('donation',)
+        model = DonationModel
+        fields = ("donation",)
+
     def __init__(self, *args, **kwargs):
         super(DonationForm, self).__init__(*args, **kwargs)
-        self.fields["donation"].widget = forms.NumberInput(attrs={"class": "form-control"})
+        self.fields["donation"].widget = forms.NumberInput(
+            attrs={"class": "form-control"}
+        )
+
+
+def validate_image_file_extension(value):
+    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
+    valid_extensions = [".jpg", ".png", ".jpeg"]
+    if not ext.lower() in valid_extensions:
+        raise ValidationError("Unsupported file extension.")
+    w, h = get_image_dimensions(value)
+    if w is None:
+        raise ValidationError("Invalid image file.")
+
+
+class PictureForm(forms.ModelForm):
+    image = forms.ImageField(
+        validators=[validate_image_file_extension],
+        error_messages={"required": "You must upload an image."},
+    )
+
+    class Meta:
+        model = PictuersModel
+        fields = ("image",)
+
+    def __init__(self, *args, **kwargs):
+        super(PictureForm, self).__init__(*args, **kwargs)
+        self.fields["image"].widget = forms.FileInput(attrs={"class": "form-control"})
--- projects/views.py
+++ projects/views.py
@@ -1,32 +1,73 @@
 from django.shortcuts import render, reverse, redirect
 from django.views import View
 from accounts.models import UserProfile
-from .models import ProjectsModel, CommentsModel, UserProjectRating
+from .models import ProjectsModel, CommentsModel, UserProjectRating, PictuersModel
 from django.contrib import messages
 from django.db.models import Avg
-
+from django import forms
 # Create your views here.
-from .forms import ProjectCreationForm, CommentForm, RatingForm, DonationForm
+from .forms import (
+    ProjectCreationForm,
+    CommentForm,
+    RatingForm,
+    DonationForm,
+    PictureForm,
+)
+
+class BasePictureFormSet(forms.BaseFormSet):
+    def clean(self):
+        super().clean()
+        
+        # Add your custom validation for minimum number of forms
+        if any(self.errors):
+            return "Please enter at least five pictures"
+
+        uploaded_pictures = len([form for form in self.forms if form.cleaned_data])
+        if uploaded_pictures < 5:
+            raise forms.ValidationError("You must upload at least 5 pictures.")
+
+PictureFormSet = forms.formset_factory(PictureForm, extra=0, min_num=5, formset=BasePictureFormSet)
 
 
 class CreateProject(View):
     def get(self, request, *args, **kwargs):
         if "profileId" not in request.session:
             return redirect(reverse("accountLogin"))
-        form = ProjectCreationForm()
-        return render(request, "projects/create.html", {"form": form})
+        project_form = ProjectCreationForm()
+        picture_formset = PictureFormSet(prefix="pictures")
+        return render(
+            request,
+            "projects/create.html",
+            {"project_form": project_form, "picture_formset": picture_formset},
+        )
 
     def post(self, request, *args, **kwargs):
         if "profileId" not in request.session:
             return redirect(reverse("accountLogin"))
-        form = ProjectCreationForm(request.POST)
-        if form.is_valid():
-            project = form.save(commit=False)
+        project_form = ProjectCreationForm(request.POST)
+        picture_formset = PictureFormSet(request.POST, request.FILES, prefix="pictures")
+        if not project_form.is_valid():
+            print(project_form.errors)
+        if not picture_formset.is_valid():
+            print(picture_formset.errors)
+        
+        if project_form.is_valid() and picture_formset.is_valid():
+            project = project_form.save(commit=False)
             project.user = UserProfile.objects.get(id=request.session["profileId"])
             project.save()
+
+            for picture_form in picture_formset:
+                picture = picture_form.save(commit=False)
+                picture.project = project
+                picture.save()
+
             return redirect(reverse("createProject"))
         else:
-            return render(request, "projects/create.html", {"form": form})
+            return render(
+                request,
+                "projects/create.html",
+                {"project_form": project_form, "picture_formset": picture_formset},
+            )
 
 
 class ProjectDetailsView(View):
